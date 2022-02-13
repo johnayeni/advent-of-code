08ec0816cfaddb2baa4a88781403d90ffbcbf192
John Ayeni <johnayeni10.aj@gmail.com>
2022-02-13 23:16:28 +0100

    day 1 part 1

---
+++ 2021/day_1/part_1.rb
@@ -0,0 +1,24 @@
+def get_input_data(file)
+  input_file = File.open(file)
+  file_data = input_file.readlines.map(&:chomp)
+  input_file.close
+  return file_data
+end
+
+
+def num_depth_measures(file)
+  file_data = get_input_data(file)
+
+  prev_measurement = nil
+  num_of_increaments = 0
+
+  file_data.each { |item|
+    if prev_measurement.is_a? Integer and item.to_i > prev_measurement
+      num_of_increaments += 1
+    end
+    prev_measurement = item.to_i
+  }
+  return num_of_increaments
+end
+
+puts num_depth_measures "input1.txt"
